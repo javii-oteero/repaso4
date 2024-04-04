1f35baa3405fc3d0c5fd3fd7ea44694e82c748aa
Javi <javii.oteero@gmail.com>
2024-04-04 14:08:02 +0200

    Todos los tests pasados

--- src/es/iesmz/tests/VectorTest.java
+++ src/es/iesmz/tests/VectorTest.java
@@ -11,4 +11,25 @@ class VectorTest {
         int[] expectedOutput = {6, 5, 4, 3, 2, 1};
         assertArrayEquals(expectedOutput, Vector.reverso(v));
     }
+
+    @Test
+    public void testReverso2() {
+        int[] v = {10, 12, 5, 221, 6, 7};
+        int[] expectedOutput = {7, 6, 221, 5, 12, 10};
+        assertArrayEquals(expectedOutput, Vector.reverso(v));
+    }
+
+    @Test
+    public void testReverso3() {
+        int[] v = {5};
+        int[] expectedOutput = {5};
+        assertArrayEquals(expectedOutput, Vector.reverso(v));
+    }
+
+    @Test
+    public void testReverso4() {
+        int[] v = {5, 10, 20};
+        int[] expectedOutput = {20, 10, 5};
+        assertArrayEquals(expectedOutput, Vector.reverso(v));
+    }
 }
